729dcf074ab889d1f1c7d351e438dfbafc731d9c
Nikita Dolgoshein <nikita01051997@gmail.com>
2025-03-22 15:03:07 +0500

    feat(brands): carousel and text for all sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import adidas from "./assets/adidas.webp";
+import slp from "./assets/slp.webp";
+import armani from "./assets/armani.webp";
+import aytao from "./assets/aytao.webp";
+import puma from "./assets/puma.webp";
+import fakoshima from "./assets/fakoshima.webp";
+import vk from "./assets/vk.webp";
+import tombogo from "./assets/tombogo.webp";
+import givenchy from "./assets/givenchy.webp";
+
+import { TEXT } from "./constants";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="p-10">
+        <h1 className="font-script text-9xl text-center">{TEXT.TITLE}</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2 className="text-3xl p-10">{TEXT.SECTIONS.BRANDS.TITLE}</h2>
+          <div className="overflow-hidden w-full relative">
+            <ul className="flex w-max marquee py-4">
+              <li>
+                <img src={adidas} className="h-32" />
+              </li>
+              <li>
+                <img src={slp} className="h-32" />
+              </li>
+              <li>
+                <img src={armani} className="h-32" />
+              </li>
+              <li>
+                <img src={aytao} className="h-32" />
+              </li>
+              <li>
+                <img src={puma} className="h-32" />
+              </li>
+              <li>
+                <img src={fakoshima} className="h-32" />
+              </li>
+              <li>
+                <img src={vk} className="h-32 grayscale" />
+              </li>
+              <li>
+                <img src={tombogo} className="h-32" />
+              </li>
+              <li>
+                <img src={givenchy} className="h-32" />
+              </li>
+              <li>
+                <img src={adidas} className="h-32" />
+              </li>
+              <li>
+                <img src={slp} className="h-32" />
+              </li>
+              <li>
+                <img src={armani} className="h-32" />
+              </li>
+              <li>
+                <img src={aytao} className="h-32" />
+              </li>
+              <li>
+                <img src={puma} className="h-32" />
+              </li>
+              <li>
+                <img src={fakoshima} className="h-32" />
+              </li>
+              <li>
+                <img src={vk} className="h-32 grayscale" />
+              </li>
+              <li>
+                <img src={tombogo} className="h-32" />
+              </li>
+              <li>
+                <img src={givenchy} className="h-32" />
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section className="p-10">
+          <h2 className="text-3xl">{TEXT.SECTIONS.SHOWS.TITLE}</h2>
+          <ol className="list-decimal">
+            <li>{TEXT.SECTIONS.SHOWS.ITEMS[0]}</li>
+            <li>{TEXT.SECTIONS.SHOWS.ITEMS[1]}</li>
+            <li>{TEXT.SECTIONS.SHOWS.ITEMS[2]}</li>
+          </ol>
+        </section>
+
+        <section className="p-10">
+          <h2 className="text-3xl">{TEXT.SECTIONS.COMMERCIALS.TITLE}</h2>
+          <ul></ul>
+        </section>
+      </main>
+
+      <footer className="text-2xl p-10">
+        <ul>
+          <li>
+            inst:{" "}
+            <a href="http://instagram.com/vac1o" target="_blank">
+              vac1o
+            </a>
+          </li>
+        </ul>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+export const TEXT = {
+  TITLE: "it's our music <3",
+  SECTIONS: {
+    BRANDS: {
+      TITLE: "Selected brands we've worked with",
+    },
+    SHOWS: {
+      TITLE: "Our View on the Diffetent Moods of Fashion Shows",
+      ITEMS: [
+        "avant-garde vibe",
+        "elegant bright indie vibe",
+        "fresh raw cold techno vibe",
+      ],
+    },
+    COMMERCIALS: {
+      TITLE: "also commercial music samples",
+    },
+  },
+};
